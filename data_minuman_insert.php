4fa046a8234337056b4f63fa204e5f73be0da9d3
evanspratama <evanspratama2@gmail.com>
2016-10-07 21:14:30 +0700

    Tambahan Tipe (not tested)
    Menambahkan insert tipe pada metode insert minuman

--- data_minuman_insert.php
+++ data_minuman_insert.php
@@ -1,12 +1,12 @@
 <!doctype HTML>
 <?php
 include "query.php";
-$data = array("","","");
+$data = array("","","","");
 if($_GET["id"] == 0){
  $data[0]="";
  $data[1]="";
  $data[2]="";
-
+ $data[3]="";
 }
 else{
 $result=get("minuman","SELECT * FROM `minuman` WHERE IdMinuman='" .$_GET["id"]. "';");
@@ -28,6 +28,14 @@ $result=get("minuman","SELECT * FROM `minuman` WHERE IdMinuman='" .$_GET["id"]. 
 <td>Stock	: </td><td><?php echo "<input type='text' name='stock' value = '" .$data[2]. "'></input>"; ?></td>
 </tr>
 <tr>
+<td>Tipe	: </td><td><?php echo " <select id='tipe'>
+  <option value=0>Botol</option>
+  <option value=1>Sloki</option>
+</select> "; 
+$data[3]=document.getElementById("tipe").options[document.getElementById("tipe").selectedIndex].value;
+?></td>
+</tr>
+<tr>
 </table>
 <input type="submit" value="Submit" name="submit"></input>
 <a href="stock_minuman.php">Kembali</a>
@@ -37,7 +45,7 @@ if(isset($_POST["submit"])){
 	$data[0]=$_POST["Nama"];
 	$data[1]=$_POST["harga"];
 	$data[2]=$_POST["stock"];
-
+	$data[3]=$_POST["tipe"];
 	if($_GET["id"] == 0){
 	insert("minuman",$data);	
 	}
